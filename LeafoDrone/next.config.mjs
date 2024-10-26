2fb122c3fb4fca05668360e7d028b5781eb41af6
swikarthanekar <143815188+swikarthanekar@users.noreply.github.com>
2024-10-26 11:32:38 +0530

    Add files via upload

---
+++ LeafoDrone/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['res.cloudinary.com'], // Allow images from Cloudinary
+    },
+  };
+  
+  export default nextConfig;
+  
