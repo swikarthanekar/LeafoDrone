31ac14363ee50cb89171191257c073d7947c4787
swikarthanekar <143815188+swikarthanekar@users.noreply.github.com>
2024-10-26 11:33:46 +0530

    Delete LeafoDrone directory

--- LeafoDrone/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['res.cloudinary.com'], // Allow images from Cloudinary
-    },
-  };
-  
-  export default nextConfig;
-  
